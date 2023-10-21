a0b64a7db0ea83de747869460f1237cdaf89a4e1
Ahmed Al-Thawadi <ahmed9althawadi@gmail.com>
2023-10-21 21:10:08 +0300

    ldap auth works now

--- src/main/java/com/notahmed/springsecurityldap/controller/security/SecurityConfiguration.java
+++ src/main/java/com/notahmed/springsecurityldap/controller/security/SecurityConfiguration.java
@@ -1,18 +1,13 @@
 package com.notahmed.springsecurityldap.controller.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.ldap.core.ContextSource;
-import org.springframework.ldap.core.support.BaseLdapPathContextSource;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.ldap.EmbeddedLdapServerContextSourceFactoryBean;
-import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
-import org.springframework.security.ldap.server.UnboundIdContainer;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,44 +16,17 @@ public class SecurityConfiguration {
 
 
 
-
     // TODO read more about EmbeddedLdapServerContextSourceFactoryBean
+    // TODO read this link https://docs.spring.io/spring-security/reference/servlet/authentication/passwords/ldap.html
     // configuration for the ldap
     // setting up the ldap server configs
-    @Bean
-    public EmbeddedLdapServerContextSourceFactoryBean contextSourceFactoryBean() {
-        EmbeddedLdapServerContextSourceFactoryBean contextSourceFactoryBean =
-                EmbeddedLdapServerContextSourceFactoryBean.fromEmbeddedLdapServer();
-
-
-        contextSourceFactoryBean.setPort(0);
-
-
-        return contextSourceFactoryBean;
-    }
-
-
-
-    // TODO read more about ldapAuthenticationManager
-    @Bean
-    public AuthenticationManager ldapAuthenticationManager(BaseLdapPathContextSource contextSource) {
-        LdapBindAuthenticationManagerFactory factory = new LdapBindAuthenticationManagerFactory(contextSource);
-
-
-        factory.setUserDnPatterns("uid={0},ou=people");
-        factory.setUserSearchBase("");
-//        factory.setUserDetailsContextMapper(new PersonContextMapper());
-        return factory.createAuthenticationManager();
-    }
-
-
-
 
 
 
 
     // authorization which is found in all projects I have worked with
     // builder pattern
+    // authentication happens here and specifying the login page
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
@@ -73,7 +41,31 @@ public class SecurityConfiguration {
 
 
 
+    // TODO read more about ldapAuthenticationManager
 
+    // this is still old syntax and will be deprecated
+    // setting up the ldap authentication
+    // same as the old version in the video
+    // so running the project will run an embedded ldap server and the spring boot project
+
+    // only people within the ou of people can log in
+    // which is bob
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=springframework,dc=org")
+                .and()
+                .passwordCompare()
+                .passwordEncoder(new LdapShaPasswordEncoder())
+                .passwordAttribute("userPassword");
+
+    }
+
+    // changing data in the ldif fixed the issue
 
 
 
